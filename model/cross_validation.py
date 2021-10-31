ce09bee58d82d9739fdb052fd543056e8d294270
Hilda Abigél Horváth <hilda.horvath@epfl.ch>
2021-10-31 11:46:30 +0100

    docstrings for cross-v.py

--- model/cross_validation.py
+++ model/cross_validation.py
@@ -1,12 +1,24 @@
 import numpy as np
 
-from scripts.proj1_helpers import *
-from dataprocess.process import *
-from scripts.implementations import *
+import os
+os.sys.path.append('../scripts')
+from proj1_helpers import *
+from implementations import *
 
+os.sys.path.append('../dataprocess')
+from dataprocess.process import *
 
 def build_k_indices(y, k_fold):
-    """build k indices for k-fold."""
+    """
+    Build k indices for k-fold.
+    
+    Inputs: 
+    y: the y vector
+    k-fold: the number of folds
+
+    Output: 
+    np.array(k_indices): the array of the splitted indicies
+    """
     num_row = y.shape[0]
     interval = int(num_row / k_fold)
     indices = np.random.permutation(num_row)
@@ -16,7 +28,23 @@ def build_k_indices(y, k_fold):
 
 
 def cross_validation(y, X, k_indices, k, degree, gamma, lambda_, max_iters, batch_size, algorithm, params):
-    """return the loss of ridge regression."""
+    """
+    Perform cross validation accoriding to the given algorithm. 
+
+    Inputs: 
+    y, X: the given dataset
+    k_indicies, k: parameters for one fold
+    gamma, lambda_, max_iters, batch_size: the usual parameters for cross validation
+    algorithm: the algorithm to perform 
+    params: the choosen data cleaning methods
+
+    Outputs:
+    acc_train: accuracy on the train set
+    acc_test: accuracy on the test set
+    y_test_pred: the predictions from the algorithm
+    y_test: the actual values of y in its test part
+    
+    """
     
     # Remove degenerated features
     X, _ = remove_tiny_features(X)
@@ -106,44 +134,63 @@ def cross_validation(y, X, k_indices, k, degree, gamma, lambda_, max_iters, batc
 
 
 def k_fold_cross_validation(y, X, k_fold, lambdas, degrees, max_iters, batch_size, gamma, algorithm, verbose, params):
-        # split data in k fold
-        k_indices = build_k_indices(y, k_fold)
+    """
+    Perform k-fold cross validation accoriding to the given algorithm. 
 
-        # define lists to store the accuracies of training data and test data
-        accs_train = np.zeros((len(degrees), len(lambdas)))
-        accs_test = np.zeros((len(degrees), len(lambdas)))
+    Inputs: 
+    y, X: the given dataset
+    k_fold: indicies for all folds
+    lambda_, max_iters, batch_size, gamma: the usual parameters for cross validation
+    algorithm: the algorithm to perform 
+    verbose: indicator of writing logs
+    params: the choosen data cleaning methods
 
-        all_preds = {}
-        all_labels = {}
-        for id_degree, degree in enumerate(degrees):
-            for id_lambda, lambda_ in enumerate(lambdas):
-                cur_acc_train = np.zeros(k_fold)
-                cur_acc_test = np.zeros(k_fold)
+    Outputs:
+    best_accuracy: the best reached value in k-fold cross validation for accuracy
+    best_degree: the best value for degree according to accuracy
+    best_lambda: the best value for lambda according to accuracy 
+    best_preds: the best reached prediction according to accuracy
+    best_labels: the best reached labels according to accuracy
+    
+    """
+    # split data in k fold
+    k_indices = build_k_indices(y, k_fold)
 
-                preds = []
-                labels = []
-                for k in range(k_fold):
-                    acc_train, acc_test, y_test_pred, y_test = cross_validation(y=y, X=X, k_indices=k_indices, k=k, 
-                                                                                degree=degree, 
-                                                                                gamma=gamma, 
-                                                                                lambda_=lambda_, 
-                                                                                max_iters=max_iters, 
-                                                                                batch_size=batch_size,
-                                                                                algorithm=algorithm,
-                                                                                params=params)
+    # define lists to store the accuracies of training data and test data
+    accs_train = np.zeros((len(degrees), len(lambdas)))
+    accs_test = np.zeros((len(degrees), len(lambdas)))
 
-                    cur_acc_train[k] = acc_train
-                    cur_acc_test[k] = acc_test
-                    preds.append(y_test_pred)
-                    labels.append(y_test)
+    all_preds = {}
+    all_labels = {}
+    for id_degree, degree in enumerate(degrees):
+        for id_lambda, lambda_ in enumerate(lambdas):
+            cur_acc_train = np.zeros(k_fold)
+            cur_acc_test = np.zeros(k_fold)
 
-                all_preds[(degree, lambda_)] = preds
-                all_labels[(degree, lambda_)] = labels
+            preds = []
+            labels = []
+            for k in range(k_fold):
+                acc_train, acc_test, y_test_pred, y_test = cross_validation(y=y, X=X, k_indices=k_indices, k=k, 
+                                                                            degree=degree, 
+                                                                            gamma=gamma, 
+                                                                            lambda_=lambda_, 
+                                                                            max_iters=max_iters, 
+                                                                            batch_size=batch_size,
+                                                                            algorithm=algorithm,
+                                                                            params=params)
 
-                accs_train[id_degree, id_lambda] = cur_acc_train.mean()
-                accs_test[id_degree, id_lambda] = cur_acc_test.mean()
-                if(verbose):
-                    print(f"Degree: {degree:2}, Lambda: {lambda_:6}: Train: {cur_acc_train.mean():.4f} +- {cur_acc_train.std():.4f}, Test: {cur_acc_test.mean():.4f} +- {cur_acc_test.std():.4f}")
+                cur_acc_train[k] = acc_train
+                cur_acc_test[k] = acc_test
+                preds.append(y_test_pred)
+                labels.append(y_test)
+
+            all_preds[(degree, lambda_)] = preds
+            all_labels[(degree, lambda_)] = labels
+
+            accs_train[id_degree, id_lambda] = cur_acc_train.mean()
+            accs_test[id_degree, id_lambda] = cur_acc_test.mean()
+            if(verbose):
+                print(f"Degree: {degree:2}, Lambda: {lambda_:6}: Train: {cur_acc_train.mean():.4f} +- {cur_acc_train.std():.4f}, Test: {cur_acc_test.mean():.4f} +- {cur_acc_test.std():.4f}")
 
         id_degree, id_lambda = np.unravel_index(np.argmax(accs_test), accs_test.shape)
         best_degree, best_lambda = degrees[id_degree], lambdas[id_lambda]
