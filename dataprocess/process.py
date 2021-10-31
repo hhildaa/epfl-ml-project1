2033992fd903b33abb341c88cfb3b4fdfce8700c
julianschnitzler <32015957+julianschnitzler@users.noreply.github.com>
2021-10-31 15:43:28 +0100

    add docstrings for process.py

--- dataprocess/process.py
+++ dataprocess/process.py
@@ -4,10 +4,31 @@ os.sys.path.append('./scripts')
 import numpy as np
 
 def remove_custom_features(X, custom_feature_ids):
+    """
+    Remove columns with given features from data
+
+    Input:
+    X -- data
+    custom_feature_ids -- list with ids of custom feature columns
+
+    returns:
+    X_cleaned -- data with custom feature columns removed
+    """
     X_cleaned = np.delete(X, custom_feature_ids, axis=1)
     return X_cleaned
 
 def remove_tiny_features(X, threshold=1):
+    """
+    Remove columns with low number of unique values
+
+    Input:
+    X -- data
+    threshold -- max number of unique values to remove column
+
+    returns:
+    X_cleaned -- data with custom feature columns removed
+    remove_features -- ids of columns that were removed
+    """
     d = X.shape[1]
     remove_features = []
     
@@ -20,7 +41,19 @@ def remove_tiny_features(X, threshold=1):
     return X_cleaned, remove_features
 
 def remove_outliers(X, y, custom_range=None):
-    """Remove outliers, i.e. rows with values that are farther than 1.5 IQR from mean"""
+    """
+    Remove outliers, i.e. rows with any value that is farther than 1.5 IQR from upper or lower quartile, resp. 
+    
+    Input:
+    X -- data without prediction variable
+    y -- prediction variable
+    custom_range -- list of (min, max) pairs to define custom ranges in which data should fall
+                    for no change, tuples can be None or custom_range can be None
+    
+    returns:
+    X -- data with rows containing an outlier removed
+    y -- prediction variable for X
+    """
     custom_keep = np.copy(X)
     if custom_range is not None:
         if len(custom_range) != X.shape[1]:
@@ -54,6 +87,16 @@ def bound_outliers(X, upper_quart=None, lower_quart=None):
     """
     Bound outliers, i.e. rows with values that are farther than 1.5 IQR
     from Lower/Upper quartile, to this quartile
+
+    Input:
+    X -- data without prediction variable
+    upper_quart -- list of upper quartiles for each column of X (None to use upper quartiles from X)
+    lower_quart -- list of lower quartiles for each column of X (None to use lower quartiles from X)
+    
+    returns:
+    X -- data with outliers bounded
+    upper_quart -- upper_quart input or list of upper quartiles for each column of X if upper_quart was None
+    lower_quart -- lower_quart input or list of lower quartiles for each column of X if lower_quart was None
     """
     if upper_quart is None:
         upper_quart = np.quantile(X, .75, axis=0)
@@ -75,11 +118,16 @@ def bound_outliers(X, upper_quart=None, lower_quart=None):
 
 def impute_median(X, train_medians=None, nan_val=-999):
     """
-        Replace nan_val by median in X
-        Custom medians to use same median for training and testing can be 
-        put into train_medians (otherwise use col medians of X)
-        
-        Returns X (median-imputed) and train_medians
+    Replace missing values in data by median
+
+    Input:
+    X -- data
+    train_medians -- list of medians for each column of X (None to use medians from X)
+    nan_val -- value to regard as missing (NaN)
+    
+    Returns: 
+    X -- data (median-imputed)
+    train_medians -- train_medians input or medians from each column of X if train_medians was None
     """
     if train_medians is None:
         train_medians = list()
